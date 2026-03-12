4c686bce30d321cee5ef6d52b364cb57f941abef
parthergk <gauravkumar81464@gmail.com>
2026-03-12 18:30:50 +0530

    make the hero section responsive

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,13 @@
 "use client"
 
-import { Facebook, Instagram, Twitter } from "lucide-react";
+import { Cross, Facebook, Instagram, Menu, Twitter, X } from "lucide-react";
 import Image from "next/image"
 import Link from "next/link"
 import { useEffect, useState } from "react"
 
 const Header = () => {
-    const [scrolled, setScrolled] = useState(false)
+    const [scrolled, setScrolled] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
 
     useEffect(() => {
         const handleScroll = () => {
@@ -21,8 +22,8 @@ const Header = () => {
         <header
             className="py-3 fixed top-0 left-0 w-full z-50 "
         >
-            <nav className={`px-10 py-2 flex items-center gap-6 border-y border-[#D1D5DB]/80 transition-all duration-300 ${scrolled ? "bg-slate-900/70 backdrop-blur-md border-b border-white/10 shadow-md mx-10 border-none rounded-xl" : "bg-transparent"}`}>
-                <div className="flex items-center gap-3 pr-6 border-r border-[#D1D5DB]/80">
+            <nav className={`px-4 sm:px-6 lg:px-10 py-1.5 lg:py-2 flex items-center justify-between gap-6 border-y-0 lg:border-y border-[#D1D5DB]/80 transition-all duration-300 ${scrolled ? "bg-slate-900/70 backdrop-blur-md border-b border-white/10 shadow-md mx-4 lg:mx-10 border-none rounded-xl" : "bg-transparent"}`}>
+                <div className="flex items-center gap-1.5 sm:gap-3 pr-6 border-r-0 lg:border-r border-[#D1D5DB]/80">
                     <Image
                         src="/logo.png"
                         alt="logo"
@@ -33,12 +34,12 @@ const Header = () => {
                         Global Group
                     </span>
                 </div>
-                <div className=" flex-1 w-full flex items-center justify-between">
-                    <ul className="hidden lg:flex items-center gap-8 text-base text-white/90">
+                <div className=" flex-1 w-full hidden lg:flex items-center justify-between">
+                    <ul className="flex items-center gap-8 text-base text-white/90">
                         <li><Link href="/">Home</Link></li>
                         <li><Link href="/about">About</Link></li>
                         <li><Link href="/divisions">Divisions</Link></li>
-                        <li><Link href="/leadership-team">Leadership & Team</Link></li>
+                        <li><Link href="/leadership-team">Leadership</Link></li>
                         <li><Link href="/partnerships">Partnerships</Link></li>
                         <li><Link href="/careers">Careers</Link></li>
                     </ul>
@@ -57,13 +58,97 @@ const Header = () => {
                         </div>
                         <Link
                             href="/contact"
-                            className="bg-white/95 text-primary px-4 py-1 rounded-md font-medium"
+                            className="bg-white/95 text-primary hover:bg-gray-200 transition px-4 py-1 rounded-md font-medium"
                         >
                             Contact
                         </Link>
                     </div>
                 </div>
+
+                <div className="lg:hidden" onClick={() => setIsOpen(true)}>
+                    <Menu className=" h-7 w-7 text-white" />
+                </div>
+
             </nav>
+
+            {isOpen && (
+                <nav className="fixed inset-0 z-50 lg:hidden bg-slate-900/90 backdrop-blur-md">
+                    <div className="flex items-center justify-between px-6 py-5 border-b border-white/10">
+
+                        <div className="flex items-center gap-2">
+                            <Image
+                                src="/logo.png"
+                                alt="logo"
+                                width={45}
+                                height={45}
+                            />
+                            <span className="text-white text-lg font-medium">
+                                Global Group
+                            </span>
+                        </div>
+
+                        <button onClick={() => setIsOpen(false)}>
+                            <X className="h-7 w-7 text-white" />
+                        </button>
+
+                    </div>
+
+                    <ul className="flex flex-col gap-6 px-6 py-10 text-lg text-white">
+
+                        <li>
+                            <Link href="/" onClick={() => setIsOpen(false)}>Home</Link>
+                        </li>
+
+                        <li>
+                            <Link href="/about" onClick={() => setIsOpen(false)}>About</Link>
+                        </li>
+
+                        <li>
+                            <Link href="/divisions" onClick={() => setIsOpen(false)}>Divisions</Link>
+                        </li>
+
+                        <li>
+                            <Link href="/leadership-team" onClick={() => setIsOpen(false)}>
+                                Leadership & Team
+                            </Link>
+                        </li>
+
+                        <li>
+                            <Link href="/partnerships" onClick={() => setIsOpen(false)}>
+                                Partnerships
+                            </Link>
+                        </li>
+
+                        <li>
+                            <Link href="/careers" onClick={() => setIsOpen(false)}>Careers</Link>
+                        </li>
+
+                    </ul>
+
+                    <div className="px-6 mt-auto pb-10">
+                        <div className="flex gap-5 mb-6">
+                            <Link href="#">
+                                <Facebook className="h-5 w-5 text-white" />
+                            </Link>
+
+                            <Link href="#">
+                                <Twitter className="h-5 w-5 text-white" />
+                            </Link>
+
+                            <Link href="#">
+                                <Instagram className="h-5 w-5 text-white" />
+                            </Link>
+                        </div>
+                        <Link
+                            href="/contact"
+                            onClick={() => setIsOpen(false)}
+                            className="block w-full text-center bg-white text-slate-900 font-medium py-2 rounded-md"
+                        >
+                            Contact
+                        </Link>
+                    </div>
+                </nav>
+            )}
         </header>
     )
 }
--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,47 +1,56 @@
 import { MoveUpRight } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Hero() {
   return (
-    <section className="relative w-full h-screen min-h-150 flex items-end px-10">  
-      <div className="absolute inset-0 -z-10">
+    <section className="relative w-full min-h-screen flex items-end px-4 sm:px-8 lg:px-10 pt-28 lg:pt-0">
+      
+      <div className="absolute inset-0 -z-10 ">
         <Image
           src="/hero.png"
           alt="Business meeting"
           fill
           priority
-          className="object-cover"
+          className="object-cover object-[65%_center] lg:object-center"
         />
       </div>
 
       <div className="absolute inset-0 bg-primary/35 -z-10"></div>
 
-        <div className="max-w-2xl text-white mb-20 lg:mb-16">
-          
-          <h1 className="text-5xl lg:text-6xl font-medium lg:font-normal leading-tight">
-            Building Businesses. <br />
-            Creating Opportunities.
-          </h1>
+      <div className="max-w-xl lg:max-w-2xl text-white mb-32 lg:mb-16">
 
-          <p className="max-w-xl mt-6 text-base text-gray-200 leading-relaxed">
-            A diversified business group building sustainable ventures and
-            creating global opportunities across multiple industries.
-          </p>
+        <h1 className="text-[28px] sm:text-4xl md:text-5xl lg:text-6xl font-medium lg:font-normal leading-tight">
+          Building  Businesses. <br />
+          Creating Opportunities.
+        </h1>
 
-          <div className="flex items-center gap-6 mt-8">
-            
-            <button className="bg-white text-primary px-4 py-2 rounded-md font-medium hover:bg-gray-200 transition">
-              Explore Our Businesses
-            </button>
+        <p className=" mt-4 sm:mt-6 text-sm sm:text-base text-gray-200 leading-relaxed">
+          A diversified business group building sustainable ventures and
+          creating global opportunities across multiple industries.
+        </p>
 
-            <button className="text-white font-medium flex items-center gap-2 hover:opacity-80 transition">
-              Contact Us
-             <MoveUpRight className=" h-4 w-4 text-white"/>
-            </button>
+        <div className="flex flex-col sm:flex-row sm:items-center gap-4 sm:gap-6 mt-5 sm:mt-8">
 
-          </div>
+          <Link
+            href="/divisions"
+            className="bg-white text-primary px-4 py-1.5 sm:py-2 rounded-md font-medium hover:bg-gray-200 transition w-fit"
+          >
+            Explore Our Businesses
+          </Link>
+
+          <Link
+            href="/contact"
+            className=" hidden text-white font-medium sm:flex items-center gap-2 hover:opacity-80 transition w-fit"
+          >
+            Contact Us
+            <MoveUpRight className="h-4 w-4 text-white" />
+          </Link>
 
         </div>
+
+      </div>
+
     </section>
   );
 }
