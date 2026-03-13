79d338ee42dcb4efa12e77fa6370e29bb8f9c7c2
parthergk <gauravkumar81464@gmail.com>
2026-03-13 17:10:17 +0530

    added the our story section in about page

--- app/(root)/about/page.tsx
+++ app/(root)/about/page.tsx
@@ -1,9 +1,13 @@
+import BusinessAreas from '@/components/about/BusinessAreas'
 import Hero from '@/components/about/Hero'
+import Story from '@/components/about/Story'
 
 const page = () => {
   return (
     <div className="w-full bg-background space-y-20">
      <Hero/>
+     <Story/>
+     <BusinessAreas/>
     </div>
   )
 }
---
+++ components/about/Story.tsx
@@ -0,0 +1,55 @@
+import Image from "next/image"
+import TagBtn from "../TagBtn"
+
+const Story = () => {
+    return (
+        <section className=" py-20 px-4 sm:px-8 lg:px-10">
+
+
+            <TagBtn name="Our Story" />
+
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 lg:gap-16 mt-6 items-center">
+
+                <div>
+                    <h1 className="text-3xl sm:text-4xl lg:text-5xl font-medium text-primary leading-tight">
+                        A Vision to Build Businesses<br />
+                        That Create Impact
+                    </h1>
+
+                    <div className="w-full h-px bg-divider my-6"></div>
+
+                    <p className=" text-text-secondary text-sm sm:text-base leading-relaxed max-w-xl">
+                        JW Global Group was established with the vision of creating
+                        innovative and scalable business ventures across multiple industries.
+                        Our focus is on identifying opportunities, building strong
+                        partnerships, and developing solutions that support long-term
+                        growth.
+                        <br/>
+                        Through strategic collaborations and a forward-thinking
+                        approach, JW Global Group continues to expand its presence
+                        while supporting entrepreneurship and global connectivity.
+                    </p>
+
+                </div>
+
+                <div className="relative lg:border-l border-divider lg:pl-10">
+
+                    <div className="relative w-full h-65 sm:h-80 lg:h-85 rounded-xl overflow-hidden">
+                        <Image
+                            src="/hero.png"
+                            alt="Business meeting"
+                            fill
+                            className="object-cover"
+                        />
+                    </div>
+
+                </div>
+
+            </div>
+
+
+        </section>
+    )
+}
+
+export default Story
